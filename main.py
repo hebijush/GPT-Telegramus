16c8004ea3e72a28140d7812ecbe9cfb5a90106c
hebijush <153817630+hebijush@users.noreply.github.com>
2024-01-20 21:42:18 +0600

    Update main.py

--- main.py
+++ main.py
@@ -39,13 +39,6 @@ from keep_alive import keep_alive
 import setup
 
 keep_alive()
-while True:
-  for bot_url in setup.bot_urls:
-    requests.head(bot_url, allow_redirects=True)
-    print('Pinged: ' + bot_url)
-  time.sleep(setup.timeout*60)
-
- 
 
 
 # GPT-Telegramus version
